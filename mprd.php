0844955a46db3e1b9d0b8f33f0bb9885be5afc94
roliod <rolandoduberu@gmail.com>
2018-04-05 23:35:42 +0100

    view from membersip periods

--- mprd.php
+++ mprd.php
@@ -57,6 +57,137 @@ function mprd_civicrm_pre($op, $objectName, $id, &$params)
 }
 
 /**
+ * Custormize contact summary page
+ *
+ *
+ * @access public
+ * @return
+ */
+function mprd_civicrm_summary($contactID, &$content, &$contentPlacement = CRM_Utils_Hook::SUMMARY_BELOW)
+{
+    // die(print_r(fetch_membership_period_with_contribution(199)));
+
+    $contentPlacement = CRM_Utils_Hook::SUMMARY_BELOW;
+    $content = '
+    <div id="customFields">
+      <div class="contact_panel">
+        <div class="contactCardLef">
+          <div class="customFieldGroup crm-collapsible ui-corner-all constituent_information crm-custom-set-block-1 collapsed">
+            <div class="collapsible-title">
+              Membership Periods
+            </div>
+            <div id="option12_wrapper" class="dataTables_wrapper no-footer">
+              <table id="option12" class="display dataTable no-footer" role="grid">
+                <thead>
+                  <tr role="row">
+                  <th class="sorting" tabindex="0" aria-controls="option12" rowspan="1" colspan="1" aria-label="Group: activate to sort column ascending">Period</th>
+                  <th class="sorting" tabindex="0" aria-controls="option12" rowspan="1" colspan="1" aria-label="Status: activate to sort column ascending">Start Date</th>
+                  <th class="sorting" tabindex="0" aria-controls="option12" rowspan="1" colspan="1" aria-label="Date Added: activate to sort column ascending">End Date</th>
+                  <th class="sorting" tabindex="0" aria-controls="option12" rowspan="1" colspan="1" aria-label="Date Added: activate to sort column ascending">Contributon Record</th>
+                  <th class="sorting_disabled" rowspan="1" colspan="1" aria-label=""></th>
+                  </tr>
+                </thead>
+                <tbody>';
+
+    foreach (fetch_membership_period_with_contribution($contactID) as $membership) {
+
+    $content .= '<tr id="group_contact" class="crm-entity odd-row odd" role="row">
+                    <td class="bold">'. $membership['period'] .'</td>
+                    <td>'. $membership['start_date']. '</td>
+                    <td>'. $membership['end_date'] .'</td>';
+
+    $contribution_id = $membership['contribution_id'];
+
+    if ($contribution_id != false) {
+
+        $url = CRM_Utils_System::url('civicrm/contact/view/contribution',
+          "reset=1&id=$contribution_id&cid=$contactID&action=view&context=contribution&selectedChild=contribute"
+        );
+
+        $content .= '<td><span><a href="'.$url.'" class="action-item crm-hover-button" title="View Contribution">View</a></span></td></tr>';
+
+    }
+
+    }
+
+    $content .=  '</tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="contactCardRight">
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>';
+}
+
+/**
+ * Get contacts membership periods, each with a contibution id
+ * Contribution id would be 0 if payment was not taken for membership or renewal
+ *
+ * @access public
+ * @return array
+ */
+function fetch_membership_period_with_contribution($contact_id)
+{
+    $sql = "SELECT period, start_date, end_date, contribution_id FROM civicrm_mprd_membership_period
+            WHERE contact_id = $contact_id ORDER BY period ASC";
+
+    $dao = CRM_Core_DAO::executeQuery( $sql, CRM_Core_DAO::$_nullArray );
+
+    $periods = array();
+
+    while($dao->fetch()) {
+        $periods[] = array(
+            'period' => $dao->period,
+            'start_date' => $dao->start_date,
+            'end_date' => $dao->end_date,
+            'contribution_id' => $dao->contribution_id
+        );
+    }
+
+    if (empty($periods)) {
+        return fetch_default_period($contact_id);
+    }
+
+    return $periods;
+}
+
+/**
+ * Get default period from membership if we dont have the contact in our custorm table
+ *
+ *
+ * @access public
+ * @return array
+ */
+function fetch_default_period($contact_id)
+{
+    $contact_sql = "SELECT start_date, end_date FROM civicrm_membership WHERE contact_id = $contact_id";
+
+    $contact = CRM_Core_DAO::executeQuery( $contact_sql, CRM_Core_DAO::$_nullArray );
+
+    $contact->fetch();
+
+    $contribution_sql = "SELECT id FROM civicrm_contribution WHERE contact_id = $contact_id";
+
+    $contribution = CRM_Core_DAO::executeQuery( $contribution_sql, CRM_Core_DAO::$_nullArray );
+
+    $contribution->fetch();
+
+    $contribution = (payment_taken_for_membership($contact_id) == true) ? $contribution->id: 0;
+
+    $period[] = array(
+        'period' => 1,
+        'start_date' => $contact->start_date,
+        'end_date' => $contact->end_date,
+        'contribution_id' => $contribution
+    );
+
+    return $period;
+}
+
+/**
  * Attach contibution id to membership period if a payment was taken
  *
  *
